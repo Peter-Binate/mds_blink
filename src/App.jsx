932f6e714e0d86075b7140aa9f4e6a4078521fc1
Peter-Binate <peter.binate@gmail.com>
2024-04-29 16:29:32 +0200

    try to use react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
 // import Header from './Header'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Navbar from './Navbar'
 import Banner from './Banner'
 import Presentation from './Presentation'
 import Features from './Features'
 import Stats from './Stats'
+import History from './History'
 import Contact from './Contact'
 import 'tailwindcss/tailwind.css';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+      <div>
+        <Navbar />
+        <Banner/>
+        <Presentation />
+        <Features />
+        <Stats />
+      </div>
+  },
+  {
+    path: "/history",
+    element: <div><History /></div>
+  },
+  {
+    path: "/contact",
+    element: <div><Contact /></div>
+  }
+])
+
 export default function App() {
-  return (
-    <div>
-      <Navbar />
-      <Banner/>
-      <Presentation />
-      <Features />
-      <Stats />
-      <Contact />
-    </div>  
-  );
+  return <RouterProvider router={router} />
 }
